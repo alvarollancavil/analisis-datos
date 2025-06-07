9e3d15580dacf83c2b6a217b15d90eb0465c69c0
alvarollnkvl <alvllnkvl@gmail.com>
2025-06-06 22:23:11 -0400

    modulo3

---
+++ modulo3/bienvenida.py
@@ -0,0 +1,3 @@
+print("Hola")
+nombre =input("Cual es tu nombre? ")
+print(f"Gusto en conocerte %s"%nombre)
---
+++ modulo3/resta.py
@@ -0,0 +1,7 @@
+print("=======================")
+print("Resta de dos numeros")
+print("=======================")
+n1=float(input("Ingresar numero 1: "))
+n2=float(input("Ingresar numero 2: "))
+r=n1-n2
+print(f"{n1:.2f} - {n2:.2f} = {r:.2f}")
